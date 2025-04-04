839974090d3a10acc199d879a5cde0fc38d9d34d
Andre Scaranto <ascaranto@gmail.com>
2025-04-03 21:22:21 -0300

    Limpar código do exemplo que não vai ser usado

--- flaskr/bilhetes.py
+++ flaskr/bilhetes.py
@@ -73,22 +73,6 @@ def consultar_bilhetes():
     return render_template('bilhetes/consultar_bilhetes.html')
 
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
