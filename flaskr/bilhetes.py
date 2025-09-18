6366d849e5eaf4cf79157895a5c02cb5de252757
Andre Scaranto <ascaranto@gmail.com>
2025-09-18 11:05:09 -0300

    Revert "Minha versão completa para revisão"
    This reverts commit b86d55f34344049ee69364de6a50bc457debb2c4.

--- flaskr/bilhetes.py
+++ flaskr/bilhetes.py
@@ -1,20 +1,9 @@
 from flask import (
-<<<<<<< HEAD
     Blueprint, flash, redirect, render_template, request, url_for
 )
 from flaskr.db import get_db
 from datetime import datetime, timedelta
 import sqlite3
-=======
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
-from werkzeug.exceptions import abort
-
-from flaskr.auth import login_required
-from flaskr.db import get_db
-from datetime import datetime
-from datetime import timedelta
->>>>>>> 32afeb09a2eb65cefe44a1447c4d6a94da14dcd1
 
 bp = Blueprint('bilhetes', __name__)
 
@@ -22,7 +11,6 @@ bp = Blueprint('bilhetes', __name__)
 def index():
     return render_template('bilhetes/index.html')
 
-<<<<<<< HEAD
 
 @bp.route('/depositar_bilhete', methods=('GET', 'POST'))
 def depositar_bilhete():
@@ -185,94 +173,6 @@ def consultar_bilhetes():
                                bilhetes_encontrados=bilhetes_encontrados)
 
     # GET
-=======
-@bp.route('/depositar_bilhete', methods=('GET', 'POST'))
-def depositar_bilhete():
-    if request.method == 'POST':
-        codigo = request.form['codigo']
-        nome = request.form['nome']
-        sobrenome = request.form['sobrenome']
-        celular = request.form['celular']
-        error = None
-
-        if not codigo:
-            error = 'Faltou o código do bilhete.'
-
-        if not nome:
-            error = 'Faltou o nome do sorteado.'
-
-        if not sobrenome:
-            error = 'Faltou o sobrenome do sorteado.'
-
-        if not celular:
-            error = 'Faltou o celular do sorteado.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            cur = db.cursor(buffered = True)
-            cur.execute(
-                'SELECT * FROM codigo WHERE codigo = %s',
-                (codigo,)
-            )
-            if cur.rowcount > 0:
-                consulta_codigo = dict(zip(cur.column_names, cur.fetchone()))
-                cur.execute('SELECT data_limite,nome FROM sorteio WHERE id_sorteio = %s',
-                           (consulta_codigo['id_sorteio'],))
-                consulta_sorteio = dict(zip(cur.column_names, cur.fetchone()))
-                data_limite = consulta_sorteio['data_limite']
-                today = datetime.today()
-                if (today - data_limite) > timedelta(days = 1):
-                    error = f"O sorteio {consulta_sorteio['nome']} não aceita mais novos bilhetes."
-                    flash(error)
-                else:
-                    try:
-                        cur.execute(
-                            'INSERT INTO bilhete (codigo,nome,sobrenome,celular,id_sorteio)'
-                            ' VALUES (%s, %s, %s, %s, %s)',
-                            (codigo,nome,sobrenome,celular,consulta_codigo['id_sorteio'])
-                        )
-                        db.commit()
-                        flash("Bilhete depositado com sucesso")
-                    except:
-                        error = f"O código {codigo} já foi utilizado."
-                        flash(error)
-            else:
-                flash("O código não é válido")
-            return redirect(url_for('bilhetes.index'))
-
-    return render_template('bilhetes/depositar_bilhete.html')
-
-@bp.route('/consultar_bilhetes', methods=('GET','POST'))
-def consultar_bilhetes():
-    if request.method == 'POST':
-        codigo = request.form['codigo']
-        celular = request.form['celular']
-        error = None
-
-        if not codigo and not celular:
-            error = 'Preencha pelo menos um campo.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            cur = db.cursor()
-            cur.execute(
-                'SELECT bilhete.id_bilhete AS id_bilhete, bilhete.codigo AS codigo, bilhete.celular AS celular,' +
-                 ' sorteio.nome AS nome_sorteio, sorteio.realizado AS realizado, sorteio.id_bilhete_sorteado as id_sorteado' + 
-                ' FROM bilhete INNER JOIN sorteio ON bilhete.id_sorteio = sorteio.id_sorteio' +
-                 ' WHERE ((%s = "") OR bilhete.codigo = %s) AND bilhete.celular LIKE %s',
-                (codigo,codigo,'%'+celular+'%')
-            )
-            bilhetes_encontrados = []
-            for resultado in cur.fetchall():
-                bilhetes_encontrados.append(dict(zip(cur.column_names, resultado)))
-            db.commit()
-            return render_template('bilhetes/resultados_consulta.html',bilhetes_encontrados=bilhetes_encontrados)
-
->>>>>>> 32afeb09a2eb65cefe44a1447c4d6a94da14dcd1
     return render_template('bilhetes/consultar_bilhetes.html')
 
 
