076e4c5e098ae75f179bbeaa7c0a6f380aeac676
guilherme-bezerra-br <guilherme.bezerra.mmi@outlook.com>
2025-09-18 19:56:02 -0300

    CSS - Página inicial
    Modificações no CSS

--- ambiente/Scripts/activate.bat
+++ ambiente/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Documents\Univesp\Projetos Integradores\Proj I\sorteio_web\ambiente
+set VIRTUAL_ENV=G:\Programação\Faculdade PI\sorteio_web-css\ambiente
 
 if not defined PROMPT set PROMPT=$P$G
 
