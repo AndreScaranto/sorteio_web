b1473b0438c91812e11383271397ef5a5f151856
Andre Scaranto <ascaranto@gmail.com>
2026-04-06 12:00:26 -0300

    Atualizar o schema da base de dados para conter um registro de produtos e de vendas.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS bilhete;
 DROP TABLE IF EXISTS codigo;
 DROP TABLE IF EXISTS sorteio;
 DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS produto;
+DROP TABLE IF EXISTS venda;
 
 CREATE TABLE administrador (
   id_admin INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -45,3 +47,17 @@ CREATE TABLE sorteio (
   FOREIGN KEY (id_bilhete_sorteado) REFERENCES bilhete(id_bilhete),
   CHECK ((NOT realizado) OR id_bilhete_sorteado)
 );
+
+CREATE TABLE produto (
+  id_produto INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT UNIQUE NOT NULL,
+  preco_atual DECIMAL(10,2) NOT NULL 
+)
+
+CREATE TABLE venda (
+  id_venda INTEGER PRIMARY KEY AUTOINCREMENT,
+  data_venda TIMESTAMP NOT NULL,
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+  FOREIGN KEY (id_produto) REFERENCES produto(id_produto),
+  preco_venda DECIMAL(10,2) NOT NULL
+)
