30308e940e71aa867489bc03222e9042ec0e02e6
Andre Scaranto <ascaranto@gmail.com>
2026-04-26 21:50:42 -0300

    Criar esquema para carregar base de dados de teste.

--- flaskr/db.py
+++ flaskr/db.py
@@ -44,6 +44,50 @@ def init_db_command():
     init_db()
     click.echo('Initialized the database.')
 
+def load_usuarios(db):
+    users = ["Andre", "Mario", "Maria", "Tamires", "Savio", "Guilherme", "Ricardo", "Armando"]
+    i = 1
+    for user in users:
+        db.execute(
+            "INSERT INTO usuario (username, password, CPF, celular) VALUES (?, ?, ?, ?)",
+            (user, generate_password_hash(user.lower()),11*str(i),"9"+8*str(i)),
+        )
+        i = i + 1
+    db.commit()
+
+def load_sorteios(db):
+    pass
+
+def load_produtos(db):
+    produtos = {"Rustico Branco" : "20", 
+                "Focaccia Gorgonzola" : "30",
+                "Focaccia Calabresa" : "27",
+                "Pão de Forma Integral" : "25"}
+    for item in produtos:
+        db.execute(
+            "INSERT INTO produto (nome, preco_atual) VALUES (?, ?)",
+            (item, produtos[item]),
+        )
+    db.commit()
+
+
+def load_vendas(db):
+    pass
+
+def load_test_db():
+    db = get_db()
+    load_usuarios(db)
+    load_produtos(db)
+    load_sorteios(db)
+    load_vendas(db)
+
+
+@click.command('load-test-db')
+def load_test_db_command():
+    """Clear the existing data and create new tables."""
+    load_test_db()
+    click.echo('Loaded the test database.')
+
 
 sqlite3.register_converter(
     "timestamp", lambda v: datetime.fromisoformat(v.decode())
@@ -52,3 +96,4 @@ sqlite3.register_converter(
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+    app.cli.add_command(load_test_db_command)
